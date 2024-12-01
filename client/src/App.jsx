aa1c40e64e5a218cdd4ca1f3a33756174e9de723
Sahil <sahilismailnavar@gmail.com>
2024-12-01 22:09:27 +0530

    Added Result component in the file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './pages/Home'
+import Result from './pages/Result'
 
 const App = () => {
   return (
     <div>
       <Home />
+      <Result />
     </div>
   )
 }
