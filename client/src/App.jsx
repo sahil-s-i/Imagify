9b112dd800aeb573b7dc0565ecd5356a1b7f9201
Sahil <sahilismailnavar@gmail.com>
2024-12-01 22:28:13 +0530

    Added routes route in the folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Result from './pages/Result'
 import BuyCredit from './pages/BuyCredit'
