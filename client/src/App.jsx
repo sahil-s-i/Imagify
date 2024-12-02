968f526a39556cc90172b671e8873b2281010528
Sahil <sahilismailnavar@gmail.com>
2024-12-02 22:33:38 +0530

    Added routes route in the folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Result from './pages/Result'
+
 import BuyCredit from './pages/BuyCredit'
 
 const App = () => {
