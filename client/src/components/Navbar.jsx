4b90e95e70bf2546c2a30582351c362a31065b8d
Sahil <sahilismailnavar@gmail.com>
2024-12-03 11:30:27 +0530

    Added logo image in the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {assets} from '../assets/assets'
 
 const Navbar = () => {
   return (
     <div>
-      
+        <img src={assets.logo} alt="logo image" />
     </div>
   )
 }
