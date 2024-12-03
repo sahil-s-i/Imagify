be445293ec03c9f31bee0e3948d338c69d448daa
Sahil <sahilismailnavar@gmail.com>
2024-12-03 11:31:15 +0530

    Added css classes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {assets} from '../assets/assets'
 const Navbar = () => {
   return (
     <div>
-        <img src={assets.logo} alt="logo image" />
+        <img src={assets.logo} alt="logo image" className='w-28 sm:w-32 lg:w-40'/>
     </div>
   )
 }
