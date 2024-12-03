58269bf1c972b62d2dfd48bdce156110ea2f8c76
Sahil <sahilismailnavar@gmail.com>
2024-12-03 11:32:30 +0530

    Added link tag

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import {assets} from '../assets/assets'
+import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <div>
-        <img src={assets.logo} alt="logo image" className='w-28 sm:w-32 lg:w-40'/>
-    </div>
-  )
+    return (
+        <div>
+            <Link to='/'>
+                <img src={assets.logo} alt="logo image" className='w-28 sm:w-32 lg:w-40' />
+            </Link>
+        </div>
+    )
 }
 
 export default Navbar
