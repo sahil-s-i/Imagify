8e5e73b806d4c8899f3f9ad334a5d3b6f98a80db
Sahil <sahilismailnavar@gmail.com>
2024-12-05 11:28:08 +0530

    Added img tag and button with credit star image

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,10 +17,14 @@ const Navbar = () => {
             <div>
                 {
                     user ?
-                        <div></div>
+                        <div>
+                            <button>
+                                <img src={assets.credit_star} alt="Credit star image" />
+                            </button>
+                        </div>
                         :
                         <div className='flex gap-2 items-center sm:gap-5'>
-                            <p onClick={()=>navigate('/buy')} className='cursor-pointer'>Pricing</p>
+                            <p onClick={() => navigate('/buy')} className='cursor-pointer'>Pricing</p>
                             <button className='bg-zinc-800 text-white py-2 px-7 text-sm rounded-full sm:px-10'>Login</button>
                         </div>
                 }
