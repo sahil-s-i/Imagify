4b825c5feafc621da3d1fcb67a611c91bd60be84
Sahil <sahilismailnavar@gmail.com>
2024-12-07 23:39:27 +0530

    Changed flex alignment in the div

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
             <div>
                 {
                     user ?
-                        <div className='flex text-center gap-2 sm:gap-3'>
+                        <div className='flex items-center gap-2 sm:gap-5'>
                             <button className='flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700 '>
                                 <img className='w-5' src={assets.credit_star} alt="Credit star image" />
                                 <p>Credit left : 50</p>
