b8902500f5acccf999dab10f43df940085e52e76
Sahil <sahilismailnavar@gmail.com>
2024-12-09 11:11:49 +0530

    Added classnames in the div

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,8 +3,8 @@ import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div>
-      <div>
+    <div className='flex flex-col justify-center items-center text-center my-20'>
+      <div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'>
         <p>Best text to image generator</p>
         <img src={assets.star_icon} alt="star icon" />
       </div>
