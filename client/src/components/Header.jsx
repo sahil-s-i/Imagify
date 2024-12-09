5b3f62d3e046425c7f1a3b39c3191fdd26e752c2
Sahil <sahilismailnavar@gmail.com>
2024-12-09 11:07:59 +0530

    Added content in the Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
     <div>
-      Header
+      <div>
+        <p>Best text to image generator</p>
+        <img src={assets.star_icon} alt="star icon" />
+      </div>
     </div>
   )
 }
