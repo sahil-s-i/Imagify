7ff94d39c3c1306415557064f8373fd4a874a6ae
Sahil <sahilismailnavar@gmail.com>
2024-12-10 23:37:05 +0530

    Added generate images button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,7 +9,14 @@ const Header = () => {
         <img src={assets.star_icon} alt="star icon" />
       </div>
       <h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'>Turn text to <span className='text-blue-600'>image</span>, in seconds.</h1>
+
       <p className='text-center max-w-xl mx-auto mt-5'>Unless your creativity with AI. Turn your imagination into visual art in seconds - just type, and watch the magic happen.</p>
+
+      <button>
+        Generate Images
+        <img src= alt="" />
+      </button>
+
     </div>
   )
 }
