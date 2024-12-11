cef90f6483f37ce998206be8c339c9c6be23f0ac
Sahil <sahilismailnavar@gmail.com>
2024-12-11 23:56:22 +0530

    Added css classname for the p tag

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -4,7 +4,7 @@ const Steps = () => {
   return (
     <div>
       <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>How it works?</h1>
-      <p>Transform Words Into Stunning Images</p>
+      <p className='text-lg text-gray-600 mb-8 '>Transform Words Into Stunning Images</p>
     </div>
   )
 }
