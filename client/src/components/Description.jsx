1c2555d121dfbfb90eda2913433c27bac0e3a7d9
Sahil <sahilismailnavar@gmail.com>
2024-12-13 19:56:09 +0530

    Added classname for h1 tag in the div

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Description = () => {
     return (
         <div className='flex flex-col items-center justify-center my-24 p-6 md:px-28'>
-            <h1>Create AI Images</h1>
+            <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Create AI Images</h1>
             <p>Turn your imagination into visuals</p>
         </div>
     )
