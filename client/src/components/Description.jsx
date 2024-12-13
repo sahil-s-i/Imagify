dd8ff4aa527029c9bd37092f42185ccfbe04e1b9
Sahil <sahilismailnavar@gmail.com>
2024-12-13 20:38:55 +0530

    Added div and img in description

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Description = () => {
     return (
         <div className='flex flex-col items-center justify-center my-24 p-6 md:px-28'>
             <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Create AI Images</h1>
             <p className='text-gray-500 mb-8'>Turn your imagination into visuals</p>
+
+
+            <div>
+                <img src={assets.sample_img_1} alt="" className='w-80 xl:w-96 rounded-lg'/>
+            </div>
         </div>
     )
 }
