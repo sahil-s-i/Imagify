10a34139aa2d2e6c975fc6bbcb7d3ac1beab926f
Sahil <sahilismailnavar@gmail.com>
2024-12-13 19:52:55 +0530

    Added clsname in the div

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Description = () => {
-  return (
-    <div>
-      <h1>Create AI Images</h1>
-      <p>Turn your imagination into visuals</p>
-    </div>
-  )
+    return (
+        <div className='flex flex-col items-center justify-center my-24 p-6 md:px-28'>
+            <h1>Create AI Images</h1>
+            <p>Turn your imagination into visuals</p>
+        </div>
+    )
 }
 
 export default Description
