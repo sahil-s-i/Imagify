45ee53b2640872ba839889948d2745d8c7331a9a
Sahil <sahilismailnavar@gmail.com>
2024-12-13 20:34:55 +0530

    Added classname for p tag in the div

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -4,7 +4,7 @@ const Description = () => {
     return (
         <div className='flex flex-col items-center justify-center my-24 p-6 md:px-28'>
             <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Create AI Images</h1>
-            <p>Turn your imagination into visuals</p>
+            <p className='text-gray-500 mb-8'>Turn your imagination into visuals</p>
         </div>
     )
 }
