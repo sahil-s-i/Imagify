33a709c86690f5a9b08f22faf2011d51d7ef5ed2
Sahil <sahilismailnavar@gmail.com>
2024-12-14 22:41:24 +0530

    Added clsnames for p tag in description

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -12,7 +12,7 @@ const Description = () => {
                 <div>
                     <h2 className='text-3xl font-medium max-w-lg mb-4'>Introducing the AI-Powered Text to Image Generator</h2>
                     <p className='text-gray-600 mb-4'>Easily bring your ideas to life with our free AI image genertor. Whether you need stunning visuals or unique imagery, our tool transforms your text into eye-catching images with just a few clicks. Imagine it, describe it, and watch it come life instantly.</p>
-                    <p>Simply type in a text prompt, and our cutting-edge AI will generate high-quality images in seconds. From product visuals to character designs and portraits, even concepts that don't yet exist can be visualized effortlessly. Powered by advanced AI technology, the creative possiblities are limitless! </p>
+                    <p className='text-gray-600'>Simply type in a text prompt, and our cutting-edge AI will generate high-quality images in seconds. From product visuals to character designs and portraits, even concepts that don't yet exist can be visualized effortlessly. Powered by advanced AI technology, the creative possiblities are limitless! </p>
                 </div>
             </div>
         </div>
