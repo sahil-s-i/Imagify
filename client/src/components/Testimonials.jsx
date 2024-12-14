2c04cd8e50095e725046afee3b476af96cf1893a
Sahil <sahilismailnavar@gmail.com>
2024-12-14 22:52:38 +0530

    Added h1 and p tag with classname

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Testimonials = () => {
-  return (
-    <div className='flex flex-col items-center justify-center my-20 py-12 md:px-28'>
-      
-    </div>
-  )
+    return (
+        <div className='flex flex-col items-center justify-center my-20 py-12 md:px-28'>
+            <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>
+                Create AI Images
+            </h1>
+            <p className='text-gray-500 mb-8'>Turn your imagination into visuals</p>
+        </div>
+    )
 }
 
 export default Testimonials
