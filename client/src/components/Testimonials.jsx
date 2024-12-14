2571d3a179cdab41c2058325ba4d5aba5db3e557
Sahil <sahilismailnavar@gmail.com>
2024-12-14 22:58:38 +0530

    Added h1 and p tag with classname

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -4,7 +4,7 @@ const Testimonials = () => {
     return (
         <div className='flex flex-col items-center justify-center my-20 py-12 md:px-28'>
             <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Customer testimonials</h1>
-            <p className='text-gray-500 mb-8'>Turn your imagination into visuals</p>
+            <p className='text-gray-500 mb-12'>What Our Users Are Saying</p>
         </div>
     )
 }
