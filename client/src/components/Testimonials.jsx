378192a6bcca04461177b72b06f7a529228e2609
Sahil <sahilismailnavar@gmail.com>
2024-12-16 23:28:47 +0530

    Added testimonialsdata and map func

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { testimonialsData } from '../assets/assets'
 
 const Testimonials = () => {
     return (
@@ -7,11 +8,9 @@ const Testimonials = () => {
             <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Customer testimonials</h1>
             <p className='text-gray-500 mb-12'>What Our Users Are Saying</p>
 
-            <div className='flex flex-col gap-6'>
+            <div className='flex flex-wrap gap-6'>
                 {testimonialsData.map((testimonial, index) => (
-                    <div key={index}>
 
-                    </div>
                 ))}
             </div>
         </div>
