ec788b6e7156d2f9069167d6247d8bf74c7f6ee5
Sahil <sahilismailnavar@gmail.com>
2024-12-18 00:48:11 +0530

    Added div and img of facebook icon

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,6 +6,10 @@ const Footer = () => {
     <div className='flex items-center justify-between gap-4 py-3 mt-20'>
         <img src={assets.logo} alt="Logo image" width={150}/>
         <p>Copyright @ 2023 | All right reserved.</p>
+
+        <div>
+            <img src={assets.facebook_icon} alt="facebook icon" width={35}/>
+        </div>
     </div>
   )
 }
