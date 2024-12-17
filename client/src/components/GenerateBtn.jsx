8540fab13cb7e7efe2ccbe4426f6f0fb4430b876
Sahil <sahilismailnavar@gmail.com>
2024-12-18 00:08:32 +0530

    Added button in the generatebtn component with img

--- client/src/components/GenerateBtn.jsx
+++ client/src/components/GenerateBtn.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const GenerateBtn = () => {
   return (
     <div>
       <h1>See the magic. Try now</h1>
+      <button>Generate Images 
+        <img src={assets.star_group} alt="star image" />
+      </button>
     </div>
   )
 }
