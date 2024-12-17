15dfd0a4a9438984109e049c6fc205a1979a4121
Sahil <sahilismailnavar@gmail.com>
2024-12-18 00:41:23 +0530

    Added img with logo in the div

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div className='flex items-center justify-between gap-4 py-3 mt-20'>
-        <img src="" alt="" />
-      
+        <img src={assets.logo} alt="Logo image" width={150}/>
     </div>
   )
 }
