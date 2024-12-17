1356323b28c0373fb4c8632cf7e2a52e4e675237
Sahil <sahilismailnavar@gmail.com>
2024-12-18 00:51:07 +0530

    Added clsname for the div of icons

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <img src={assets.logo} alt="Logo image" width={150}/>
         <p>Copyright @ 2023 | All right reserved.</p>
 
-        <div>
+        <div className='flex gap-2.5'>
             <img src={assets.facebook_icon} alt="facebook icon" width={35}/>
             <img src={assets.instagram_icon} alt="instagram icon" width={35}/>
             <img src={assets.twitter_icon} alt="twitter icon" width={35}/>
