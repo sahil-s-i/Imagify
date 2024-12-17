fbe12f61b791254d1271c77ed128c6f37eb2bdf1
Sahil <sahilismailnavar@gmail.com>
2024-12-18 00:10:30 +0530

    Added clsname for h1 and img tag

--- client/src/components/GenerateBtn.jsx
+++ client/src/components/GenerateBtn.jsx
@@ -4,9 +4,9 @@ import { assets } from '../assets/assets'
 const GenerateBtn = () => {
   return (
     <div>
-      <h1>See the magic. Try now</h1>
+      <h1 className='text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold text-neutral-800 py-6 md:py-16'>See the magic. Try now</h1>
       <button>Generate Images 
-        <img src={assets.star_group} alt="star image" />
+        <img src={assets.star_group} alt="star image" className='h-6' />
       </button>
     </div>
   )
