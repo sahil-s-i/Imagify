107ed1e329c1e2361e3228ae46911295c18000da
Sahil <sahilismailnavar@gmail.com>
2024-12-18 00:53:45 +0530

    Added clsname for p tag in div

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='flex items-center justify-between gap-4 py-3 mt-20'>
         <img src={assets.logo} alt="Logo image" width={150}/>
-        <p className='flex-1 border-1 border-gray-400'>Copyright @ 2023 | All right reserved.</p>
+        <p className='flex-1 border-1 border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden:'>Copyright @ 2023 | All right reserved.</p>
 
         <div className='flex gap-2.5'>
             <img src={assets.facebook_icon} alt="facebook icon" width={35}/>
