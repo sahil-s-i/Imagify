55a77c78c148f46857e72410f50dc834abafe543
Sahil <sahilismailnavar@gmail.com>
2024-12-18 00:44:42 +0530

    Added p tagin the div

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <div className='flex items-center justify-between gap-4 py-3 mt-20'>
         <img src={assets.logo} alt="Logo image" width={150}/>
+        <p>Copyright @ 2023 | All right reserved.</p>
     </div>
   )
 }
