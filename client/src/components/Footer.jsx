459110fd533fd50654b1e134cbbed8237f3f9504
Sahil <sahilismailnavar@gmail.com>
2024-12-18 00:39:33 +0530

    Added intial code in footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex items-center justify-between gap-4 py-3 mt-20'>
+        <img src="" alt="" />
+      
+    </div>
+  )
+}
+
+export default Footer
