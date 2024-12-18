e1429d7e34a13ab13acd0a9cd4d7fa7671e9d9b9
Sahil <sahilismailnavar@gmail.com>
2024-12-19 00:27:09 +0530

    Added loading text with p tag

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -8,7 +8,10 @@ const Result = () => {
         <img src={assets.sample_img_1} alt="" className='max-w-sm rounded' />
         <span className='absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]' />
       </div>
+      <p>Loading.....</p>
     </div>
+
+    
   )
 }
 
