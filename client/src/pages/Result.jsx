db5ac56a686c69252bb0e18927d742259a36805d
Sahil <sahilismailnavar@gmail.com>
2024-12-19 01:02:08 +0530

    Added clsname for input field

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -12,7 +12,7 @@ const Result = () => {
         <p>Loading.....</p>
       </div>
       <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full'>
-        <input type="text" placeholder='Describe what you want to generate' className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20' />
+        <input type="text" placeholder='Describe what you want to generate' className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color' />
         <button className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full' type='submit'>Generate</button>
       </div>
     </form>
