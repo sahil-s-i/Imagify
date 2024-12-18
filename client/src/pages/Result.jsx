dbffd27da37fa092693ea7eef186e5763cffb7da
Sahil <sahilismailnavar@gmail.com>
2024-12-19 00:23:47 +0530

    Added clsname and span tag in the div

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -5,7 +5,8 @@ const Result = () => {
   return (
     <div>
       <div className='relative'>
-          <img src={assets.sample_img_1} alt="" />
+        <img src={assets.sample_img_1} alt="" className='max-w-sm rounded' />
+        <span className='absolute bottom-0 left-0 h-1 bg-blue-500' />
       </div>
     </div>
   )
