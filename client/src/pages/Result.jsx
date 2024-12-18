980978d71dce2acc6adc36025c22270230889551
Sahil <sahilismailnavar@gmail.com>
2024-12-19 00:28:55 +0530

    Added form tag to add multiple divs

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -3,15 +3,19 @@ import { assets } from '../assets/assets'
 
 const Result = () => {
   return (
-    <div>
-      <div className='relative'>
-        <img src={assets.sample_img_1} alt="" className='max-w-sm rounded' />
-        <span className='absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]' />
+    <form>
+      <div>
+        <div className='relative'>
+          <img src={assets.sample_img_1} alt="" className='max-w-sm rounded' />
+          <span className='absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]' />
+        </div>
+        <p>Loading.....</p>
       </div>
-      <p>Loading.....</p>
-    </div>
 
-    
+      <div>
+
+      </div>
+    </form>
   )
 }
 
