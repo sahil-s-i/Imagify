3272072690a972d4c29e81fe1cba10a3bd7cc926
Sahil <sahilismailnavar@gmail.com>
2024-12-20 23:25:07 +0530

    Added isImageLoaded to render divs depends on the conditions

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 
 const Result = () => {
 
-  const [image,setImage] = useState(assets.sample_img_1)
+  const [image, setImage] = useState(assets.sample_img_1)
   const [isImageLoaded, setIsImageLoaded] = useState(false)
 
   return (
@@ -15,10 +15,12 @@ const Result = () => {
         </div>
         <p>Loading.....</p>
       </div>
-      <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full'>
-        <input type="text" placeholder='Describe what you want to generate' className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color' />
-        <button className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full' type='submit'>Generate</button>
-      </div>
+      {!isImageLoaded &&
+        <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full'>
+          <input type="text" placeholder='Describe what you want to generate' className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color' />
+          <button className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full' type='submit'>Generate</button>
+        </div>
+      }
 
       <div className='flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full'>
         <p className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>Generate another</p>
