66f4d901d0c85fc635154f0dc7ed308729c7e102
Sahil <sahilismailnavar@gmail.com>
2024-12-22 17:56:39 +0530

    Added clsname for the btn and div

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const BuyCredit = () => {
   return (
-    <div>
-      <button>Our Plans</button>
+    <div className='min-h-[80vh] text-center pt-14 mb-10'>
+      <button className='border border-gray-400 px-10 py-2 rounded-full mb-6'>Our Plans</button>
       <h1>Choose the plan</h1>
     </div>
   )
