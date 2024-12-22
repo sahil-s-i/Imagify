be388088beeaaeec3ec0f95ad6ffb3034c1da22f
Sahil <sahilismailnavar@gmail.com>
2024-12-22 17:59:41 +0530

    Added map function in the new created div

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { plans } from '../assets/assets'
 
 const BuyCredit = () => {
   return (
     <div className='min-h-[80vh] text-center pt-14 mb-10'>
       <button className='border border-gray-400 px-10 py-2 rounded-full mb-6'>Our Plans</button>
       <h1 className='text-center text-3xl font-medium mb-6 sm:mb-10'>Choose the plan</h1>
+
+      <div>
+        {plans.map((item,index)=>(
+          
+        ))}
+      </div>
     </div>
+
   )
 }
 
