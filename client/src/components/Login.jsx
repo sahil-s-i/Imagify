318653ec3b13fc84961092f0e8b64941262b31d2
Sahil <sahilismailnavar@gmail.com>
2024-12-24 11:25:16 +0530

    Added clsname for the form tag

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Login = () => {
     return (
         <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
-            <form>
+            <form className='relative bg-white p-10 rounded-xl text-slate-500'>
                 <h1>Sign Up</h1>
                 <p>Welcome back! Please sign in to continue</p>
 
