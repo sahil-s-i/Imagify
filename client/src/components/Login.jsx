4c298ccaa2ae76edbac56cad2a17a988f5ae400a
Sahil <sahilismailnavar@gmail.com>
2024-12-24 11:16:05 +0530

    Added clsname in the div

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
+            
+        </div>
+    )
 }
 
 export default Login
