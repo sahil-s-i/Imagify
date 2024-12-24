028fe046efff8f0e6b7fbf1d05e191ca2dead97c
Sahil <sahilismailnavar@gmail.com>
2024-12-24 12:00:55 +0530

    Added state variable data in the div and also added functionality for the full name input field div which is to hide when user is login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -8,13 +8,13 @@ const Login = () => {
     return (
         <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
             <form className='relative bg-white p-10 rounded-xl text-slate-500'>
-                <h1 className='text-center text-2xl text-neutral-700 font-medium'>Sign Up</h1>
+                <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state}</h1>
                 <p className='text-sm'>Welcome back! Please sign in to continue</p>
 
-                <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
+                {state !== 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
                     <img width={30} src={assets.profile_icon} alt="User icon" />
                     <input className='outline-none text-sm' type="text" placeholder='Full Name' required />
-                </div>
+                </div>}
 
                 <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                     <img width={20} src={assets.email_icon} alt="Email icon" />
