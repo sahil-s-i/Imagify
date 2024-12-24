c0c65094ea890b28bc20cc269604d02b581d0108
Sahil <sahilismailnavar@gmail.com>
2024-12-24 11:27:39 +0530

    Added clsname for h1 and p tag

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,8 +5,8 @@ const Login = () => {
     return (
         <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
             <form className='relative bg-white p-10 rounded-xl text-slate-500'>
-                <h1>Sign Up</h1>
-                <p>Welcome back! Please sign in to continue</p>
+                <h1 className='text-center text-2xl text-neutral-700 font-medium'>Sign Up</h1>
+                <p className='text-sm'>Welcome back! Please sign in to continue</p>
 
                 <div>
                     <img src={assets.user_icon} alt="User icon" />
