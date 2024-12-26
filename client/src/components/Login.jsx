a77cb9dccf0263fada89cf5f64b288c442870224
Sahil <sahilismailnavar@gmail.com>
2024-12-26 19:01:43 +0530

    Added useEffect in the code

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 
 const Login = () => {
 
     const [state, setState] = useState('Login');
+
+    useEffect(()=>{
+        document.body.style.overflow = 'hidden';
+    },[])
 
     return (
         <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
