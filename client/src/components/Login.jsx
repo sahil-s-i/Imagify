d7980c2641b932a50a6c6a10e2e29de31f1cf6d4
Sahil <sahilismailnavar@gmail.com>
2024-12-26 17:53:25 +0530

    Added tertiary op to login or create account

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
 
                 <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password?</p>
 
-                <button className='bg-blue-600 w-full text-white py-2 rounded-full'>Create an account</button>
+                <button className='bg-blue-600 w-full text-white py-2 rounded-full'>{state === 'Login' ? 'login' : 'create account'}</button>
 
                 <p className='mt-5 text-center'>Don't have an account?<span className='text-blue-600 cursor-pointer'> Sign Up</span></p>
                 <p className='mt-5 text-center'>Already have an account?<span className='text-blue-600 cursor-pointer'> Login</span></p>
