f7b5daa170e891e457f2fbe5d503519497f3ad22
Sahil <sahilismailnavar@gmail.com>
2024-12-26 19:14:03 +0530

    Added useState showlogin and setShowlogin

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,16 +1,16 @@
-import { createContext,useState } from "react";
+import { createContext, useState } from "react";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-    const [user, Setuser] = useState(null);
+    const [user, setUser] = useState(null);
     const [showLogin, setShowLogin] = useState(false);
-    
+
     const value = {
-        user,Setuser 
+        user, setUser, showLogin, setShowLogin
     }
 
-    return(
+    return (
         <AppContext.Provider value={value}>
             {props.children}
         </AppContext.Provider>
