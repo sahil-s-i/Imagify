ace7cd40658de9f7c415e0baadc09c47a9f43a80
Sahil <sahilismailnavar@gmail.com>
2024-12-26 19:02:46 +0530

    Added useEffect in the code

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,9 +5,13 @@ const Login = () => {
 
     const [state, setState] = useState('Login');
 
-    useEffect(()=>{
+    useEffect(() => {
         document.body.style.overflow = 'hidden';
-    },[])
+
+        return () => {
+            document.body.style.overflow = 'unset';
+        }
+    }, [])
 
     return (
         <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
