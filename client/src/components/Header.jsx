9b115bca8185067b891e1b1828bbf3de5c5c6947
Sahil <sahilismailnavar@gmail.com>
2024-12-28 16:40:51 +0530

    Added animation in the p tag

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -53,7 +53,11 @@ const Header = () => {
         ))}
       </motion.div>
 
-      <p className='mt-2 text-neutral-600'>Generated images from imagify</p>
+      <motion.p 
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1.2, duration: 0.8 }}
+      className='mt-2 text-neutral-600'>Generated images from imagify</motion.p>
 
     </motion.div>
   )
