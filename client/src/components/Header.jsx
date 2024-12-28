bb8c69739b4d0088665175d02a4c80e65a01f9e4
Sahil <sahilismailnavar@gmail.com>
2024-12-28 16:43:30 +0530

    Added onclickHandler func in Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
         transition={{ delay: 0.2, duration: 0.8 }}
       >Unless your creativity with AI. Turn your imagination into visual art in seconds - just type, and watch the magic happen.</motion.p>
 
-      <motion.button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'
+      <motion.button onClick={onClickHandler} className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
         initial={{ opacity: 0 }}
@@ -53,11 +53,11 @@ const Header = () => {
         ))}
       </motion.div>
 
-      <motion.p 
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 1.2, duration: 0.8 }}
-      className='mt-2 text-neutral-600'>Generated images from imagify</motion.p>
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.2, duration: 0.8 }}
+        className='mt-2 text-neutral-600'>Generated images from imagify</motion.p>
 
     </motion.div>
   )
