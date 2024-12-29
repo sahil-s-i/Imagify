5df67a2a3c550024ccdd167bdd90c0133c2c08a0
Sahil <sahilismailnavar@gmail.com>
2024-12-29 22:58:04 +0530

    Added onclickhandler and onclick in the btn

--- client/src/components/GenerateBtn.jsx
+++ client/src/components/GenerateBtn.jsx
@@ -9,6 +9,14 @@ const GenerateBtn = () => {
   const { user, setShowLogin } = useContext(AppContext)
   const navigate = useNavigate()
 
+  const onClickHandler = () => {
+    if (user) {
+      navigate('/result')
+    } else {
+      setShowLogin(true)
+    }
+  }
+
   return (
     <motion.div
       initial={{ opacity: 0.2, y: 100 }}
@@ -17,7 +25,7 @@ const GenerateBtn = () => {
       viewport={{ once: true }}
       className='pb-16 text-center'>
       <h1 className='text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold text-neutral-800 py-6 md:py-16'>See the magic. Try now</h1>
-      <button className='inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-105 transition-all duration-500'>Generate Images
+      <button onClick={onClickHandler} className='inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-105 transition-all duration-500'>Generate Images
         <img src={assets.star_group} alt="star image" className='h-6' />
       </button>
     </motion.div>
