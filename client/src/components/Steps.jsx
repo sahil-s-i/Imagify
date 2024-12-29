c564f478eec10d95f75bd9df186593716c6e1cf5
Sahil <sahilismailnavar@gmail.com>
2024-12-29 13:58:59 +0530

    Added animation in the div

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -4,14 +4,19 @@ import { motion } from 'framer-motion'
 
 const Steps = () => {
   return (
-    <div className='flex flex-col items-center justify-center my-32'>
+    <motion.div
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      className='flex flex-col items-center justify-center my-32'>
       <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>How it works?</h1>
       <p className='text-lg text-gray-600 mb-8'>Transform Words Into Stunning Images</p>
 
       <div className='space-y-4 w-full max-w-3xl text-sm'>
         {stepsData.map((item, index) => (
-          <div className='flex items-center gap-4 p-5 px-8 bg-white/20 shadow-md border cursor-pointer hover:scale-[1.02] transition-all duration-300 rounded-lg' 
-          key={index}>
+          <div className='flex items-center gap-4 p-5 px-8 bg-white/20 shadow-md border cursor-pointer hover:scale-[1.02] transition-all duration-300 rounded-lg'
+            key={index}>
             <img src={item.icon} alt="Icon image" />
             <div>
               <h2 className='text-xl font-medium'>{item.title}</h2>
@@ -20,7 +25,7 @@ const Steps = () => {
           </div>
         ))}
       </div>
-    </div>
+    </motion.div>
   )
 }
 
