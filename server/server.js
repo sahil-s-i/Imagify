7307ccfedc9c555d3670b7db3541a1bc3c3d1065
Sahil <sahilismailnavar@gmail.com>
2024-12-31 23:05:39 +0530

    Added .get for home to check api

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 
-const PORT =  process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json())
 app.use(cors)
+
+app.get('/', (req, res) => res.send("API Working"))
