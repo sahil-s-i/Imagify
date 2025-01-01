b4f8e049c8e14daa51a201d2660cd1aac59e5e35
Sahil <sahilismailnavar@gmail.com>
2025-01-01 23:27:03 +0530

    Imported connectDB in the server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 
+import connectDB from './config/mongodb.js'
+
 const PORT = process.env.PORT || 4000;
 const app = express();
 
