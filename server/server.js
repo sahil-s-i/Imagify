04a7c0d8147e19c2a57ca2c89dd4a19008a23f88
Sahil <sahilismailnavar@gmail.com>
2025-01-06 22:46:29 +0530

    Modified code lines

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './config/mongodb.js'
+import connectDB from './config/mongodb.js';
 
 const PORT = process.env.PORT || 4000;
 const app = express()
