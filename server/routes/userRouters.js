42d25481b744ef4b786af5f93de81079d5d80830
Sahil <sahilismailnavar@gmail.com>
2025-01-13 23:07:11 +0530

    Added middleware userAuth in code

--- server/routes/userRouters.js
+++ server/routes/userRouters.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { loginUser, registerUser, userCredits } from '../controllers/userController';
+import userAuth from '../middlewares/auth';
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
-userRouter.post('/credits', userCredits)
+userRouter.post('/credits', userAuth, userCredits)
 
 export default userRouter
 
