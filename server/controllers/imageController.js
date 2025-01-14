97f97fd986cf0addd3b3fdc7ab5c9c1e7b8fab6d
Sahil <sahilismailnavar@gmail.com>
2025-01-14 20:43:36 +0530

    Added code for fetching data

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -17,10 +17,11 @@ export const generateImage = async (req, res) => {
         const formData = new FormData()
         formData.append('prompt', prompt)
 
-        await axios.post('https://clipdrop-api.co/text-to-image/v1', formData, {
+        const { data } = await axios.post('https://clipdrop-api.co/text-to-image/v1', formData, {
             headers: {
-                'x-api-key': YOUR_API_KEY,
+                'x-api-key': process.env.CLIPDROP_API,
             },
+            responseType: 'arraybuffer'
         })
 
     } catch (error) {
