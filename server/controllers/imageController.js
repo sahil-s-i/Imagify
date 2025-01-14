e483e770438960cc002b112a79e78a1eb723062b
Sahil <sahilismailnavar@gmail.com>
2025-01-14 20:33:50 +0530

    Imported form-data and realted code

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -1,5 +1,5 @@
 import userModel from "../models/userModel";
-
+import FormData from "form-data";
 
 export const generateImage = async (req, res) => {
     try {
@@ -12,7 +12,10 @@ export const generateImage = async (req, res) => {
         if (user.creditBalance === 0 || userModel.creditBalance < 0) {
             return res.json({ success: false, message: 'No credit Balance', creditBalance: user.creditBalance });
         }
-        
+
+        const formData = new FormData()
+        formData.append('prompt', prompt)
+
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: error.message })
