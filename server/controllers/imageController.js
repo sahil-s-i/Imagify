b8281745b449418e9b143e20a6a6f34774a3fe21
Sahil <sahilismailnavar@gmail.com>
2025-01-14 19:48:38 +0530

    Added try catch blck and try blck code

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -1,8 +1,13 @@
+import userModel from "../models/userModel";
 
 
 export const generateImage = async (req, res) => {
     try {
+        const { userId, prompt } = req.body;
+        const user = await userModel.findById(userId);
+        if (!user) {
 
+        }
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: error.message })
