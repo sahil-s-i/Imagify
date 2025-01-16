c1e29807c2254f47af522a6d8e1aa52952bc8cac
Sahil <sahilismailnavar@gmail.com>
2025-01-16 22:18:55 +0530

    Added useState component in the code

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,11 +5,14 @@ export const AppContext = createContext();
 const AppContextProvider = (props) => {
     const [user, setUser] = useState(null);
     const [showLogin, setShowLogin] = useState(false);
+    const [token, setToken] = useState(localStorage.getItem('token'));
+
+    const [credit, setCredit] = useState(false);
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const value = {
-        user, setUser, showLogin, setShowLogin, backendUrl
+        user, setUser, showLogin, setShowLogin, backendUrl, token, setToken
     }
 
     return (
