3b82f103b991c07dbc49afe83a3389c430da7c5a
Sahil <sahilismailnavar@gmail.com>
2025-01-16 22:25:34 +0530

    Added onclick for setting password in input field

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -38,12 +38,12 @@ const Login = () => {
 
                 <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                     <img width={20} src={assets.email_icon} alt="Email icon" />
-                    <input onClick={e => setEmail(e.target.value)} value={name} className='outline-none text-sm' type="email" placeholder='Email Id' required />
+                    <input onClick={e => setEmail(e.target.value)} value={email} className='outline-none text-sm' type="email" placeholder='Email Id' required />
                 </div>
 
                 <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                     <img width={15} src={assets.lock_icon} alt="Lock icon" />
-                    <input className='outline-none text-sm' type="password" placeholder='Password' required />
+                    <input onClick={e => setPassword(e.target.value)} value={password} className='outline-none text-sm' type="password" placeholder='Password' required />
                 </div>
 
                 <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password?</p>
