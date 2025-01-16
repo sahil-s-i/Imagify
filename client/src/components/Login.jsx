b9c9c3a3e1c495d6d495a5dcba3599e43da4d352
Sahil <sahilismailnavar@gmail.com>
2025-01-16 22:24:35 +0530

    Added onclick for setting email in input field

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
 
                 <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                     <img width={20} src={assets.email_icon} alt="Email icon" />
-                    <input className='outline-none text-sm' type="email" placeholder='Email Id' required />
+                    <input onClick={e => setEmail(e.target.value)} value={name} className='outline-none text-sm' type="email" placeholder='Email Id' required />
                 </div>
 
                 <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
