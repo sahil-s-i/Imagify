781900acec2effc62663136a24a882c62bf04ece
Sahil <sahilismailnavar@gmail.com>
2025-01-18 23:46:57 +0530

    Added toastContainer in the code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
 
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
-      <ToastContainer />
+      <ToastContainer position='bottom-right'/>
       <Navbar />
       {showLogin && <Login />}
       <Routes>
