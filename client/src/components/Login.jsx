5d5785482f12e17230566ccbccbe4253a70d2cc3
Sahil <sahilismailnavar@gmail.com>
2025-01-18 23:52:05 +0530

    Added code for the user not logged in and the registration have to do

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -28,6 +28,17 @@ const Login = () => {
                 } else {
                     toast.error(data.message)
                 }
+            } else {
+                const { data } = await axios.post(backendUrl + '/api/user/register', { name, email, password });
+
+                if (data.success) {
+                    setToken(data.token);
+                    setUser(data.user)
+                    localStorage.getItem('token', data.token)
+                    setShowLogin(false);
+                } else {
+                    toast.error(data.message)
+                }
             }
         } catch (error) {
 
