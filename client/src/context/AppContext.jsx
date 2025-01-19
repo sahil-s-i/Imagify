beccd95c1266eb679e3efb1c71de729987f74c57
Sahil <sahilismailnavar@gmail.com>
2025-01-19 21:18:14 +0530

    modified code in the loadCreditsData

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -17,6 +17,10 @@ const AppContextProvider = (props) => {
         try {
             const { data } = await axios.get(backendUrl + '/api/user/credits', { headers: { token } })
             
+            if(data.success){
+                setCredit(data.credits);
+                setUser(data.user);
+            }
         } catch (error) {
             console.log(error);
             toast.error(error.message);
