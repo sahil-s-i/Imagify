b5eb8911767fb94ee5aecc7300b8243320e89148
Sahil <sahilismailnavar@gmail.com>
2025-01-21 21:49:38 +0530

    Added logout func with onclick in the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
                                 <img src={assets.profile_icon} className='w-10 drop-shadow' alt="Profile image" />
                                 <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
                                     <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
-                                        <li className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
+                                        <li onClick={logout} className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
                                     </ul>
 
                                 </div>
