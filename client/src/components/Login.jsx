503638354138e7488a5e21ec2f549b01d2cc478a
Sahil <sahilismailnavar@gmail.com>
2025-01-22 22:57:40 +0530

    Edited the code

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
                 if (data.success) {
                     setToken(data.token);
                     setUser(data.user);
-                    localStorage.getItem('token', data.token)
+                    localStorage.getItem('token', data.token);
                     setShowLogin(false);
                 } else {
                     toast.error(data.message)
