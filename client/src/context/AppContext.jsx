9bea68e394224625d421a6f73e393af00465e19a
Sahil <sahilismailnavar@gmail.com>
2025-01-24 23:35:02 +0530

    Changed in generate image func

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -32,7 +32,7 @@ const AppContextProvider = (props) => {
 
     const generateImage = async (prompt) => {
         try {
-            const {data} = await axios.post(backendUrl + '/api.image/generate-image', { prompt }, { headers: { token } });
+            const {data} = await axios.post(backendUrl + '/api/image/generate-image', { prompt }, { headers: { token } });
 
             if(data.success){
                 loadCreditsData()
