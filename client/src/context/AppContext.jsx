4537514349de5908dec06b2a99f1458c4adcbcc9
Sahil <sahilismailnavar@gmail.com>
2025-01-24 23:29:45 +0530

    Imported useNavigate from r-dom

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { toast } from 'react-toastify'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export const AppContext = createContext();
 
