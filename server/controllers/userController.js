b321a164b61ae544915e6b834ec060ec26ed549e
Sahil <sahilismailnavar@gmail.com>
2025-01-25 22:48:57 +0530

    Added razorpayInstance

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -71,7 +71,7 @@ const userCredits = async (res, req) => {
 
 const razorpayInstance = new razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET
+    key_secret: process.env.RAZORPAY_KEY_SECRET 
 })
 
 
