b12fc68d2d6fed94b6ded395e32070383670ce3f
Sahil <sahilismailnavar@gmail.com>
2025-01-25 22:23:19 +0530

    modified appcontextprovider link

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import AppContextProvider from './context/appContext.jsx'
+import AppContextProvider from './context/AppContext.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
