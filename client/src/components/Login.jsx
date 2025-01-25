399bdecc22583d3d3b434a043735424d1364602b
Sahil <sahilismailnavar@gmail.com>
2025-01-25 22:24:20 +0530

    Modified onclick to onchnage

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -66,17 +66,17 @@ const Login = () => {
 
                 {state !== 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
                     <img width={30} src={assets.profile_icon} alt="User icon" />
-                    <input onClick={e => setName(e.target.value)} value={name} className='outline-none text-sm' type="text" placeholder='Full Name' required />
+                    <input onChange={e => setName(e.target.value)} value={name} className='outline-none text-sm' type="text" placeholder='Full Name' required />
                 </div>}
 
                 <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                     <img width={20} src={assets.email_icon} alt="Email icon" />
-                    <input onClick={e => setEmail(e.target.value)} value={email} className='outline-none text-sm' type="email" placeholder='Email Id' required />
+                    <input onChange={e => setEmail(e.target.value)} value={email} className='outline-none text-sm' type="email" placeholder='Email Id' required />
                 </div>
 
                 <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
                     <img width={15} src={assets.lock_icon} alt="Lock icon" />
-                    <input onClick={e => setPassword(e.target.value)} value={password} className='outline-none text-sm' type="password" placeholder='Password' required />
+                    <input onChange={e => setPassword(e.target.value)} value={password} className='outline-none text-sm' type="password" placeholder='Password' required />
                 </div>
 
                 <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot Password?</p>
