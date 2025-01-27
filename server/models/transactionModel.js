d29352bdeb67fab91edd210f805df26cf25a3118
Sahil <sahilismailnavar@gmail.com>
2025-01-27 23:18:02 +0530

    Added data types and required information in transactionSchema

--- server/models/transactionModel.js
+++ server/models/transactionModel.js
@@ -5,7 +5,8 @@ const transactionSchema = new mongoose.Schema({
     plan: { type: String, required: true },
     amount: { type: Number, required: true },
     credits: { type: Number, required: true },
-    payment: { type: Boolean, default: false }
+    payment: { type: Boolean, default: false },
+    date : {type:Number},
 });
 
 const transactionModel = mongoose.models.transaction || mongoose.model("transaction", transactionSchema);
