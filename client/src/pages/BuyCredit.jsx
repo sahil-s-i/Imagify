e888658c9883a33390396d5ae1118354f8d077ae
Sahil <sahilismailnavar@gmail.com>
2025-01-29 23:50:44 +0530

    Added options in initpy

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -14,10 +14,16 @@ const BuyCredit = () => {
 
   const initpy = async (order) => {
     const options = {
-      key : import.meta.env.VITE_RAZORPAY_KEY_ID,
-      amount : order.amount,
-      currency:  order.currency,
-      
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency,
+      name: "Credits Payment",
+      description: "Credits Payment",
+      order_id: order.id,
+      receipt: order.receipt,
+      handler: async (response) => {
+        
+      }
     }
   }
 
