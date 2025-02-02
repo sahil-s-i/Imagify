1d15c168bf3f30fa7a90a2dd19e9b9d4c0ffe660
Sahil <sahilismailnavar@gmail.com>
2025-02-02 23:13:49 +0530

    exported verifyRazorpay function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -166,4 +166,4 @@ const verifyRazorpay = async (req, res) => {
     }
 }
 
-export { registerUser, loginUser, userCredits, paymentRazorpay }
+export { registerUser, loginUser, userCredits, paymentRazorpay, verifyRazorpay }
