b9833053ec1d709ab138aa57ca12a1b9d7312918
Sahil <sahilismailnavar@gmail.com>
2025-02-02 23:16:33 +0530

    Added route for verify razor pay

--- server/routes/userRouters.js
+++ server/routes/userRouters.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { loginUser, paymentRazorpay, registerUser, userCredits } from '../controllers/userController.js';
+import { loginUser, paymentRazorpay, registerUser, userCredits, verifyRazorpay } from '../controllers/userController.js';
 import userAuth from '../middlewares/auth.js';
 
 const userRouter = express.Router();
@@ -8,7 +8,7 @@ userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
 userRouter.get('/credits', userAuth, userCredits)
 userRouter.post('/pay-razor', userAuth, paymentRazorpay)
-userRouter.post('/verify-razor', )
+userRouter.post('/verify-razor', verifyRazorpay)
 
 export default userRouter
 
