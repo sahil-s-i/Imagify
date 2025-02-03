712489e14a0e953288ec2523342592f042f7563b
Sahil <sahilismailnavar@gmail.com>
2025-02-03 20:41:08 +0530

    Added route for verify razor pay

--- server/routes/userRouters.js
+++ server/routes/userRouters.js
@@ -10,7 +10,6 @@ userRouter.get('/credits', userAuth, userCredits)
 userRouter.post('/pay-razor', userAuth, paymentRazorpay)
 userRouter.post('/verify-razor', verifyRazorpay)
 
-
 export default userRouter
 
 // localhost : 4000/api/user/register
