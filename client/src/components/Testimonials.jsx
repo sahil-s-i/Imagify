88a5ec877f1c98df17f938a4fe222eacc9201730
Sahil <sahilismailnavar@gmail.com>
2025-02-07 23:56:18 +0530

    Strutured the code lines

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -17,7 +17,6 @@ const Testimonials = () => {
                 {testimonialsData.map((testimonial, index) => (
                     <div key={index} className='bg-white/20 p-12 rounded-lg shadow-md order w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all'>
                         <div className='flex flex-col items-center'>
-                            
                             <img src={testimonial.image} className='rounded-full w-14' alt="testimonials image" />
                             <h2 className='text-xl font-semibold mt-3'>{testimonial.name}</h2>
                             <p className='text-gray-500 mb-4'>{testimonial.role}</p>
